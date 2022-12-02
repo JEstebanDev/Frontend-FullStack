eb800599040d8142484dac77d8fa30b174ae7792
JEstebanDev <castanoesteban9@gmail.com>
2022-12-02 08:15:21 -0500

    finished the final project frontend

--- ProjectTypeScript/src/main.tsx
+++ ProjectTypeScript/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Dashboard from "./src/dashboard";
+import Error from "./src/error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Dashboard />,
+    errorElement: <Error />,
   },
 ]);
 
